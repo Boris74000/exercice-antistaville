6bfdb931c62e136b774eb33c5f17dc2368cffac9
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-07-29 23:13:18 +0200

    Code commenté

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Menu mobile
+// ------------- Menu mobile -------------
 document.getElementById("burger-icon").addEventListener("click", function () {
     document.getElementById("main-menu").classList.remove("hidden-menu-mobile");
     document.getElementById("main-menu").classList.add("display-menu-mobile");
@@ -10,8 +10,9 @@ document.getElementById("close-menu-mobile").addEventListener("click", function 
     document.getElementById("main-menu").classList.remove("display-menu-mobile");
     document.getElementById("main-menu").classList.add("hidden-menu-mobile");
 })
+// --------------------------------------
 
-// Search feature
+// ------------- Search feature ----------
 document.getElementById("magnifier-icon").addEventListener("click", function () {
     document.getElementById("search-block").classList.remove("hidden-search-block");
     document.getElementById("search-block").classList.add("display-search-block");
@@ -21,8 +22,9 @@ document.getElementById("close-search-block").addEventListener("click", function
     document.getElementById("search-block").classList.remove("display-search-block");
     document.getElementById("search-block").classList.add("hidden-search-block");
 })
+// -------------------------------------
 
-// Accordion
+// ------------- Accordion --------------
 const accordion = document.getElementsByClassName('label');
 
 for (let i= 0; i < accordion.length; i++) {

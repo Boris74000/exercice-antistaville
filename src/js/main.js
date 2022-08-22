dc8c817673969d176a5d86714a24a8d8965c1b96
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-08-22 20:53:09 +0200

    Optimizing script

--- src/js/main.js
+++ src/js/main.js
@@ -2,39 +2,43 @@
 
 // ------------- Menu mobile -------------
 document.getElementById("burger-icon").addEventListener("click", function () {
-    document.getElementById("main-menu").classList.remove("hidden-menu-mobile");
-    document.getElementById("main-menu").classList.add("display-menu-mobile");
+    document.getElementById("main-menu").classList.toggle("display-menu-mobile");
 })
 
 document.getElementById("close-menu-mobile").addEventListener("click", function () {
-    document.getElementById("main-menu").classList.remove("display-menu-mobile");
-    document.getElementById("main-menu").classList.add("hidden-menu-mobile");
+    document.getElementById("main-menu").classList.toggle("display-menu-mobile");
+
 })
 // --------------------------------------
 
 // ------------- Search feature ----------
 document.getElementById("magnifier-icon").addEventListener("click", function () {
-    document.getElementById("search-block").classList.remove("hidden-search-block");
-    document.getElementById("search-block").classList.add("display-search-block");
+    document.getElementById("search-block").classList.toggle("display-search-block");
 })
 
 document.getElementById("close-search-block").addEventListener("click", function () {
-    document.getElementById("search-block").classList.remove("display-search-block");
-    document.getElementById("search-block").classList.add("hidden-search-block");
+    document.getElementById("search-block").classList.toggle("display-search-block");
 })
 // -------------------------------------
 
 // ------------- Accordion --------------
-const accordion = document.getElementsByClassName('label');
+const accordionHeaders = document.getElementsByClassName('accordion-header');
+Array.prototype.forEach.call(accordionHeaders, accordionHeader => {
+    let target = accordionHeader.parentElement.nextElementSibling;
 
-for (let i= 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
-        this.classList.toggle('item-accordion-active');
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
+    accordionHeader.onclick = () => {
+
+        if (target.style.maxHeight) {
+            target.style.maxHeight = null;
         } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
+            target.style.maxHeight = target.scrollHeight + "px";
         }
-    })
-}
+        accordionHeader.parentNode.classList.toggle('rotate-arrow');
+        accordionHeader.parentNode.classList.toggle('border-bottom-accordion');
+        target.classList.toggle('border-bottom-accordion')
+        let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
+        let hidden = target.getAttribute('aria-hidden') === 'true' || false;
+        accordionHeader.setAttribute('aria-expanded', !expanded);
+        target.setAttribute('aria-hidden', !hidden);
+    }
+})
